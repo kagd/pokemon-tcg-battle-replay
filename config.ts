ffb4cffec218b301c1af0a3397947a65cc489477
Grant Klinsing <klingra@chrobinson.com>
2025-04-09 19:45:27 -0500

    Move config to root

---
+++ config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  apiKey: "B2quTyhbQ9IZUajjtqBsxuNvXEbfpLq3UJQ0ZrMxn2eoEuiWWUzcJQQJ99BCACHYHv6XJ3w3AAAAACOG0DFS",
+  apiVersion: "2024-04-01-preview",
+  endpoint: "https://ai-sbx-services-iuxlny.openai.azure.com/",
+  deployment: "gpt-4o",
+  modelName: "gpt-4o",
+}
